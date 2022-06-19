9fc349cb9461bea7735665529ce411dc507ae783
Ackyras <45270846+Ackyras@users.noreply.github.com>
2022-06-19 18:42:01 +0700

    update base subject controller

--- app/Http/Controllers/Admin/SubjectController.php
+++ app/Http/Controllers/Admin/SubjectController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Subject\StoreSubjectRequest;
 use App\Models\Subject;
 use Illuminate\Http\Request;
 
@@ -22,6 +23,7 @@ class SubjectController extends Controller
     public function create()
     {
         //
+        return view('admin.DataMaster.Subjects.create');
     }
 
     /**
@@ -30,18 +32,32 @@ class SubjectController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(StoreSubjectRequest $request)
     {
         //
+        $validated = $request->validated();
+        $subject = Subject::create($validated);
+        if ($subject) {
+            return to_route('admin.data.master.subject.index')->with(
+                [
+                    'success'   =>  'Mata Kuliah baru berhasil dibuat'
+                ]
+            );
+        }
+        return back()->with(
+            [
+                'failed'   =>  'Mata Kuliah baru gagal dibuat'
+            ]
+        );
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  int  Subject $subject
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Subject $subject)
     {
         //
     }
@@ -49,34 +65,48 @@ class SubjectController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  int  Subject $subject
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Subject $subject)
     {
         //
+        return view('admin.DataMaster.Subjects.edit', compact('subject'));
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  int  Subject $subject
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Subject $subject)
     {
         //
+        $validated = $request->validated();
+        Subject::create($validated);
+        return to_route('admin.data.master.subject.index')->with(
+            [
+                'success'   =>  'Mata Kuliah baru berhasil dibuat'
+            ]
+        );
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  int  Subject $subject
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Subject $subject)
     {
         //
+        $subject->deleteOrFail();
+        return to_route('admin.data.master.subject.index')->with(
+            [
+                'success'   =>  'Mata Kuliah berhasil dihapus'
+            ]
+        );
     }
 }
