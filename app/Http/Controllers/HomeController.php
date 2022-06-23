f0750fba692557e2946bdd9b3678877c5f44f2fb
Ackyras <45270846+Ackyras@users.noreply.github.com>
2022-06-23 07:10:25 +0700

    make website index

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Period;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $periods = Period::orderBy('id', 'desc')->get();
+        return view('website.pages.home.index', compact('periods'));
     }
 }
