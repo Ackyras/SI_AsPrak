3587c5b92a0c507087e0f633b2c24f782a2f0313
Ackyras <45270846+Ackyras@users.noreply.github.com>
2022-06-30 03:53:53 +0700

    remove unused obj

--- app/Http/Controllers/User/UserDashboardController.php
+++ app/Http/Controllers/User/UserDashboardController.php
@@ -9,15 +9,19 @@ use App\Models\PeriodSubjectRegistrar;
 
 class UserDashboardController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $user = auth()->user()->registrar;
-        $period_subject_registrar = PeriodSubjectRegistrar::where('registrar_id',$user->id)
-            ->with('period_subject.subject')
-            ->get();
-        // dd($user,$period_subject_registrar);
+        $user->load('period_subjects');
+        // dd($user);
+        // $subjects = $user->period_subjects;
+        // dd($subjects);
+        // $period_subject_registrar = PeriodSubjectRegistrar::where('registrar_id', $user->id)
+        //     ->with('period_subject.subject')
+        //     ->get();
+        // dd($user, $period_subject_registrar);
         return Inertia::render('Dashboard', [
             'user'      => $user,
-            'period_subject_registrar'  => $period_subject_registrar
         ]);
     }
 }
