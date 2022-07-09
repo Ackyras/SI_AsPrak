2be19f3b93643c991663ee8d438a50edb313410b
Ackyras <45270846+Ackyras@users.noreply.github.com>
2022-07-09 13:16:09 +0700

    add filtering based on subject

--- resources/views/admin/pages/practicum/assistant/index-per-subject.blade.php
+++ resources/views/admin/pages/practicum/assistant/index-per-subject.blade.php
@@ -5,37 +5,45 @@
     <div class="card">
         <div class="card-header">
             <div class="d-flex align-items-center justify-content-between">
-                <h2 class="card-title font-weight-bold">Data Seluruh Asisten Praktikum</h2>
+                <h2 class="card-title font-weight-bold">Data Asisten Praktikum Mata Kuliah {{
+                    $period_subject->subject->name }}</h2>
             </div>
         </div>
 
         <div class="card-body">
             <div id="subject_assistant_table_wrapper" class="dataTables_wrapper dt-bootstrap4">
-                <div class="btn-group mb-2">
-                    <button style="background-color: #9ca3af;" type="button" class="btn dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                    Filter Data
+                <div class="mb-2 btn-group">
+                    <button style="background-color: #9ca3af;" type="button" class="btn dropdown-toggle"
+                        data-toggle="dropdown" aria-expanded="false">
+                        Filter Data
                     </button>
                     <div class="dropdown-menu">
-                    <a href="{{ route('admin.practicum.lab-assistant.index') }}"
-                        class="dropdown-item {{ request()->routeIs('admin.practicum.lab-assistant.index') ? 'bg-info' : '' }}">
-                        Berdasarkan Nama Asisten
-                    </a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="{{ route('admin.berdasarkan-kelas-xxx-yyy-zzz') }}">Mata Kuliah A</a>
-                    <a class="dropdown-item" href="{{ route('admin.berdasarkan-kelas-xxx-yyy-zzz') }}">Mata Kuliah B</a>
-                    <a class="dropdown-item" href="{{ route('admin.berdasarkan-kelas-xxx-yyy-zzz') }}">Mata Kuliah C</a>
+                        <a href="{{ route('admin.practicum.lab-assistant.index') }}"
+                            class="dropdown-item {{ request()->routeIs('admin.practicum.lab-assistant.index') ? 'bg-info' : '' }}">
+                            Berdasarkan Nama Asisten
+                        </a>
+                        <div class="dropdown-divider"></div>
+                        @forelse ($subjects as $subject)
+                        <a class="dropdown-item"
+                            href="{{ route('admin.practicum.lab-assistant.subject-based', ['period_subject_id'=> $subject->pivot->id]) }}">
+                            {{ $subject->name }}
+                        </a>
+                        @empty
+
+                        @endforelse
                     </div>
                 </div>
-                <table id="subject_assistant_table" class="table table-bordered table-hover dataTable dtr-inline collapsed"
+                <table id="subject_assistant_table"
+                    class="table table-bordered table-hover dataTable dtr-inline collapsed"
                     aria-describedby="subject_assistant_table_info">
                     <thead>
                         <tr>
-                            <th style="text-align: center" tabindex="0" aria-controls="subject_assistant_table" rowspan="1"
-                                colspan="1">Nama</th>
-                            <th style="text-align: center" tabindex="0" aria-controls="subject_assistant_table" rowspan="1"
-                                colspan="1">NIM</th>
-                            <th style="text-align: center" tabindex="0" aria-controls="subject_assistant_table" rowspan="1"
-                                colspan="1">
+                            <th style="text-align: center" tabindex="0" aria-controls="subject_assistant_table"
+                                rowspan="1" colspan="1">Nama</th>
+                            <th style="text-align: center" tabindex="0" aria-controls="subject_assistant_table"
+                                rowspan="1" colspan="1">NIM</th>
+                            <th style="text-align: center" tabindex="0" aria-controls="subject_assistant_table"
+                                rowspan="1" colspan="1">
                                 Keaktifan
                             </th>
                             <th tabindex="0" aria-controls="period_subject_table" rowspan="1" colspan="1"
@@ -44,36 +52,27 @@
                     </thead>
 
                     <tbody>
+                        @forelse ($lab_assistants as $lab_assistant)
                         <tr class="">
-                            <td tabindex="0">Marihot</td>
-                            <td>118140XXX</td>
-                            <td>69%</td>
+                            <td tabindex="0">{{ $lab_assistant->name }}</td>
+                            <td>{{ $lab_assistant->nim }}</td>
+                            <td>
+                                @if ($lab_assistant->schedules_count > 0)
+                                {{ $lab_assistant->presences_count /
+                                $lab_assistant->schedules_count * 100}}%
+                                @else
+                                Belum ada jadwal ditentukan
+                                @endif
+                            </td>
                             <td>
                                 <div class="d-flex align-items-center justify-content-center">
-                                    <a role="button" href=""class="btn btn-sm btn-success">Lihat Detail</a>
+                                    <a role="button" href="" class="btn btn-sm btn-success">Lihat Detail</a>
                                 </div>
                             </td>
                         </tr>
-                        <tr class="">
-                            <td tabindex="0">Maria</td>
-                            <td>118140YYY</td>
-                            <td>79%</td>
-                            <td>
-                                <div class="d-flex align-items-center justify-content-center">
-                                    <a role="button" href=""class="btn btn-sm btn-success">Lihat Detail</a>
-                                </div>
-                            </td>
-                        </tr>
-                        <tr class="">
-                            <td tabindex="0">Maringan</td>
-                            <td>118140ZZZ</td>
-                            <td>89%</td>
-                            <td>
-                                <div class="d-flex align-items-center justify-content-center">
-                                    <a role="button" href=""class="btn btn-sm btn-success">Lihat Detail</a>
-                                </div>
-                            </td>
-                        </tr>
+                        @empty
+
+                        @endforelse
                     </tbody>
                     <tfoot>
                         <tr>
