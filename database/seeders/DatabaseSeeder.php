1afe8a73ae84238c6a120b29ea51dc95893fdbaa
Ackyras <45270846+Ackyras@users.noreply.github.com>
2022-07-10 14:25:58 +0700

    add qrcode seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -37,6 +37,7 @@ class DatabaseSeeder extends Seeder
             RoomSeeder::class,
             ClassroomSeeder::class,
             ScheduleSeeder::class,
+            QrCodeSeeder::class,
         ]);
     }
 }
--- database/seeders/QrCodeSeeder.php
+++ database/seeders/QrCodeSeeder.php
@@ -2,8 +2,12 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Qr;
+use App\Models\Period;
+use Illuminate\Support\Str;
+use App\Models\PeriodSubject;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class QrCodeSeeder extends Seeder
 {
@@ -15,5 +19,37 @@ class QrCodeSeeder extends Seeder
     public function run()
     {
         //
+        $period = Period::query()
+            ->where('is_active', true)
+            ->with(
+                [
+                    'subjects',
+                ],
+            )
+            ->first()
+            //
+        ;
+        foreach ($period->subjects as $subject) {
+            $period_subject = PeriodSubject::query()
+                ->where('id', $subject->pivot->id)
+                ->with('classrooms.schedule')
+                ->first()
+                //
+            ;
+            foreach ($period_subject->classrooms as $classroom) {
+                $token = '';
+                do {
+                    $token = Str::random(16);
+                } while (Qr::where('token', $token)->first() instanceof Qr);
+                $qr = Qr::create(
+                    [
+                        'token'         =>  $token,
+                        'end_date'      =>  now()->addDay(),
+                        'schedule_id'   =>  $classroom->schedule->id,
+                    ]
+                );
+            }
+        }
+        // $qr = Qr::create()
     }
 }
