e4f800ea1b1adfa1705d59505a9d55f12e5755f2
Ackyras <45270846+Ackyras@users.noreply.github.com>
2022-07-19 09:11:46 +0700

    add restriction for exam

--- app/Http/Controllers/Website/NewsController.php
+++ app/Http/Controllers/Website/NewsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Website;
 
 use App\Http\Controllers\Controller;
 use App\Models\Period;
+use App\Models\PeriodSubjectRegistrar;
 use Illuminate\Http\Request;
 
 class NewsController extends Controller
@@ -20,7 +21,21 @@ class NewsController extends Controller
     public function file_selection_over(Period $period)
     {
         $period->load('subjects');
-        return view('website.pages.news.file_selection', ['period' => $period]);
+        $psrs = PeriodSubjectRegistrar::query()
+            ->whereRelation('period_subject', 'period_id', $period->id)
+            ->where('is_pass_file_selection', true)
+            ->with(
+                [
+                    'period_subject'   => function ($query) use ($period) {
+                        $query->where('period_id', $period->id);
+                    },
+                    'registrar',
+                    'period_subject.subject'
+                ]
+            )->get()
+            //
+        ;
+        return view('website.pages.news.file_selection', compact('psrs'), ['period' => $period]);
     }
 
     public function exam_selection_over(Period $period)
