991a04f5dc1bc6818d02c1da990baa798b2f782e
Ackyras <45270846+Ackyras@users.noreply.github.com>
2022-07-21 15:21:20 +0700

    pass psr data to schedule

--- app/Http/Controllers/User/UserDashboardController.php
+++ app/Http/Controllers/User/UserDashboardController.php
@@ -6,6 +6,7 @@ use Inertia\Inertia;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Period;
+use App\Models\PeriodSubject;
 use App\Models\PeriodSubjectRegistrar;
 use App\Models\Presence;
 use App\Models\Qr;
@@ -94,19 +95,44 @@ class UserDashboardController extends Controller
     {
         $period = Period::firstWhere('is_active', true);
         $user = auth()->user()->registrar;
-        $psr = PeriodSubjectRegistrar::query()
+        $psrs = PeriodSubjectRegistrar::query()
             ->where('registrar_id', $user->id)
             ->where('is_pass_exam_selection', true)
             ->where('is_pass_file_selection', true)
             ->with(
                 [
                     'period_subject'    =>  function ($query) use ($user, $period) {
-                        $query->where('period_id', $period)->with('subject');
+                        $query->where('period_id', $period->id)->with(
+                            [
+                                'classrooms.schedule'
+                            ]
+                        );
                     }
                 ]
-            );
+            )->get();
+        $period_subjects = PeriodSubject::query()
+            ->where('period_id', $period->id)
+            ->whereRelation('registrars', 'registrars.id', $user->id)
+            ->whereRelation('registrars', 'psr.is_pass_file_selection', true)
+            ->whereRelation('registrars', 'psr.is_pass_exam_selection', true)
+            ->with(
+                [
+                    'subject',
+                    'classrooms.schedule',
+                    'registrars'    =>  function ($query) use ($user) {
+                        $query->where('registrars.id', $user->id);
+                    }
+                ]
+            )
+            ->get()
+            // ->dd()
+            //
+        ;
+        // dd($period_subjects, $psrs);
         return Inertia::render('Schedule/Index', [
-            'user'  =>  $user
+            'user'  =>  $user,
+            'period_subjects'   =>  $period_subjects,
+            'psrs'  =>  $psrs
         ]);
     }
 }
--- resources/js/Pages/Schedule/Index.vue
+++ resources/js/Pages/Schedule/Index.vue
@@ -1,12 +1,19 @@
 <template>
-    <Authenticated >
+    <Authenticated>
         <div class="min-h-screen">
-            <p class="px-4 my-3 text-lg text-center md:text-left uppercase tracking-wide text-emerald-600 font-bold">Jadwal Praktikum Saya</p>
-            
+            <p
+                class="px-4 my-3 text-lg text-center md:text-left uppercase tracking-wide text-emerald-600 font-bold"
+            >
+                Jadwal Praktikum Saya
+            </p>
+
             <div class="grid grid-cols-1 lg:grid-cols-3 gap-4">
-                <card-subject-schedule v-for="subject in subjects" :subject-data="subject" />
+                <card-subject-schedule
+                    v-for="(subject, key) in period_subjects"
+                    :subject-data="subject"
+                    :psr-data="psrs[key]"
+                />
             </div>
-            
         </div>
     </Authenticated>
 </template>
@@ -16,39 +23,41 @@ import CardSubjectSchedule from "@/components/Cards/CardSubjectSchedule.vue";
 
 export default {
     name: "dashboard-page",
-    data(){
-        return{
-            subjects : [
+    data() {
+        return {
+            subjects: [
                 {
-                    name        : "Rekayasa Perangkat Lunak",
-                    schedule    : 'Senin, 22:22 - 23:23', 
-                    class       : 'RB', 
-                    isSelected  : true,
-                    id          : 1
+                    name: "Rekayasa Perangkat Lunak",
+                    schedule: "Senin, 22:22 - 23:23",
+                    class: "RB",
+                    isSelected: true,
+                    id: 1,
                 },
                 {
-                    name        : "Rekayasa Perangkat Keras",
-                    schedule    : '',
-                    class       : '',
-                    isSelected  : false,
-                    id          : 2
+                    name: "Rekayasa Perangkat Keras",
+                    schedule: "",
+                    class: "",
+                    isSelected: false,
+                    id: 2,
                 },
                 {
-                    name        : "Rekayasa Manusia",
-                    schedules   : 'Rabu, 22:22 - 23:23',
-                    class       : 'C',
-                    isSelected  : false,
-                    id          : 3
+                    name: "Rekayasa Manusia",
+                    schedules: "Rabu, 22:22 - 23:23",
+                    class: "C",
+                    isSelected: false,
+                    id: 3,
                 },
-            ]
+            ],
         };
     },
     components: {
         Authenticated,
-        CardSubjectSchedule
+        CardSubjectSchedule,
     },
     props: {
         user: Object,
+        period_subjects: Object,
+        psrs: Object,
     },
 };
 </script>
