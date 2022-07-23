b474ea76fef1e8d1d49332af9cec8ce3958f51da
Ackyras <45270846+Ackyras@users.noreply.github.com>
2022-07-23 13:26:25 +0700

    test view

--- resources/js/Components/Cards/CardSubjectSchedule.vue
+++ resources/js/Components/Cards/CardSubjectSchedule.vue
@@ -3,30 +3,25 @@
         <p class="text-base font-semibold text-emerald-600 mb-2">
             {{ subjectData.period_subject.subject.name }}
         </p>
-        <p class="text-sm text-gray-400 mb-2" v-if="subjectData.schedules.length <= 0">
+        <p
+            class="text-sm text-gray-400 mb-2"
+            v-if="subjectData.schedules.length <= 0"
+        >
             Anda belum memilih jadwal
         </p>
         <div class="grid grid-cols-3 gap-2 text-sm text-gray-500 mb-3" v-else>
-            <p class="font-bold col-span-3">
-                Jadwal terdaftar :
-            </p>
-            <p class="col-span-1">
-                Kelas
-            </p>
+            <p class="font-bold col-span-3">Jadwal terdaftar :</p>
+            <p class="col-span-1">Kelas</p>
             <p class="col-span-2 font-semibold">
                 {{ subjectData.schedules[0].classroom.name }}
             </p>
-            <p class="col-span-1">
-                Jadwal
-            </p>
+            <p class="col-span-1">Jadwal</p>
             <p class="col-span-2 font-semibold">
                 {{ subjectData.schedules[0].day }},
                 {{ subjectData.schedules[0].start_time }} -
                 {{ subjectData.schedules[0].end_time }}
             </p>
-            <p class="col-span-1">
-                Ruangan
-            </p>
+            <p class="col-span-1">Ruangan</p>
             <p class="col-span-2 font-semibold">
                 {{ subjectData.schedules[0].room.building }},
                 {{ subjectData.schedules[0].room.name }}
@@ -45,17 +40,27 @@
             </p> -->
         </div>
 
-
-
         <form @submit.prevent="submit">
             <div class="mb-2">
-                <select class="w-full text-sm text-gray-600 py-1 px-2 border-none bg-gray-50" v-model="form.schedule">
-                    <option selected disabled hidden value="-1">Pilih Jadwal</option>
+                <select
+                    class="w-full text-sm text-gray-600 py-1 px-2 border-none bg-gray-50"
+                    v-model="form.schedule"
+                >
+                    <option selected disabled hidden value="-1">
+                        Pilih Jadwal
+                    </option>
 
-                    <option v-for="classroom in subjectData.period_subject.classrooms" :value="classroom.schedule.id"
-                        :selected="(subjectData.schedules.length > 0) && (subjectData.schedules[0].id === classroom.schedule.id)">
-                        {{ classroom.name }} -
-                        {{ classroom.schedule.day }},
+                    <option
+                        v-for="classroom in subjectData.period_subject
+                            .classrooms"
+                        :value="classroom.schedule.id"
+                        :selected="
+                            subjectData.schedules.length > 0 &&
+                            subjectData.schedules[0].id ===
+                                classroom.schedule.id
+                        "
+                    >
+                        {{ classroom.name }} - {{ classroom.schedule.day }},
                         {{ classroom.schedule.start_time }} -
                         {{ classroom.schedule.end_time }}
                         ({{ classroom.schedule.room.building }},
@@ -63,14 +68,25 @@
                     </option>
                 </select>
             </div>
-            <button type="submit"
-                :disabled="(current_schedule === form.schedule)"
-                class="py-1 px-2 rounded text-white font-bold text-sm mb-2" :class="{
-                    'bg-sky-400 hover:bg-sky-500': (subjectData.schedules.length > 0) && (current_schedule !== form.schedule),
-                    'bg-sky-200': (subjectData.schedules.length > 0) && (current_schedule === form.schedule),
-                    'bg-emerald-400 hover:bg-emerald-500': (subjectData.schedules.length <= 0) && (current_schedule !== form.schedule),
-                    'bg-emerald-200': (subjectData.schedules.length <= 0) && (current_schedule === form.schedule),
-                }">
+            <button
+                type="submit"
+                :disabled="current_schedule === form.schedule"
+                class="py-1 px-2 rounded text-white font-bold text-sm mb-2"
+                :class="{
+                    'bg-sky-400 hover:bg-sky-500':
+                        subjectData.schedules.length > 0 &&
+                        current_schedule !== form.schedule,
+                    'bg-sky-200':
+                        subjectData.schedules.length > 0 &&
+                        current_schedule === form.schedule,
+                    'bg-emerald-400 hover:bg-emerald-500':
+                        subjectData.schedules.length <= 0 &&
+                        current_schedule !== form.schedule,
+                    'bg-emerald-200':
+                        subjectData.schedules.length <= 0 &&
+                        current_schedule === form.schedule,
+                }"
+            >
                 {{
                     subjectData.schedules.length > 0
                         ? "Ajukan Perubahan Jadwal"
@@ -94,11 +110,11 @@ export default {
     props: {
         subjectData: Object,
     },
-    setup(props){
+    setup(props) {
         const current_schedule = computed(() => {
-            if(props.subjectData.schedules.length <= 0){
-                return '-1';
-            }else{
+            if (props.subjectData.schedules.length <= 0) {
+                return "-1";
+            } else {
                 return props.subjectData.schedules[0].id;
             }
         });
@@ -107,16 +123,13 @@ export default {
         });
 
         const submit = () => {
-            Inertia.post(
-                route("test-post"),
-                form
-            );
+            Inertia.post(route("user.schedule.store"), form);
         };
         return {
             current_schedule,
             form,
-            submit
-        }
-    }
+            submit,
+        };
+    },
 };
 </script>
