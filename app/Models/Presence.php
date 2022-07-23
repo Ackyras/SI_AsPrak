2cae6396b57e56157236371ca131c614c914f0a4
Ackyras <45270846+Ackyras@users.noreply.github.com>
2022-07-23 13:24:22 +0700

    add functional feature for presence model

--- app/Models/Presence.php
+++ app/Models/Presence.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Presence extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'psr_id',
+        'qr_id',
+        'is_valid'
+    ];
+
+    public function psr()
+    {
+        return $this->belongsTo(PeriodSubjectRegistrar::class, 'psr_id', 'id');
+    }
+
+    public function qr()
+    {
+        return $this->belongsTo(Qr::class, 'qr_id', 'id');
+    }
 }
