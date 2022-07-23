ffd429dcbe3c41cc87760c81685fb85a17645ccd
Ackyras <45270846+Ackyras@users.noreply.github.com>
2022-07-23 13:24:46 +0700

    finisihing presence

--- app/Http/Controllers/User/UserDashboardController.php
+++ app/Http/Controllers/User/UserDashboardController.php
@@ -10,6 +10,7 @@ use App\Models\PeriodSubject;
 use App\Models\PeriodSubjectRegistrar;
 use App\Models\Presence;
 use App\Models\Qr;
+use App\Models\Schedule;
 use App\Models\User;
 use Illuminate\Support\Facades\Session;
 
@@ -24,71 +25,142 @@ class UserDashboardController extends Controller
         return Inertia::render('Dashboard', [
             'user'  =>  $user
         ])
-            ->with(
-                [
-                    'alert'   =>  Session::has('alert') ?
-                        Session::get('alert', 'default')
-                        :
-                        [
-                            'msg'       =>  'Welcome back,' . $user->name,
-                            'status'    =>  'success',
-                        ],
-                ]
-            );
+            // ->with(
+            //     [
+            //         'alert'   =>  Session::has('alert') ?
+            //             Session::get('alert', 'default')
+            //             :
+            //             [
+            //                 'msg'       =>  'Welcome back,' . $user->name,
+            //                 'status'    =>  'success',
+            //             ],
+            //     ]
+            // )
+        ;
     }
 
     public function presence(Request $request)
     {
+        $state = false;
+        // validating token
         $validated = $request->validate(
             [
                 'token' =>  'required',
             ]
         );
 
+        // finding qr with token
         $qr = Qr::query()
             ->where('token', $validated['token'])
             ->with(
                 [
                     'schedule'  =>  [
-                        'classroom.period_subject'
+                        'classroom.period_subject.subject'
                     ],
                 ]
             )
             ->first();
+
+        // return failed if qr is not found
         if (!$qr) {
-            return back()->with(
+            return to_route('user.dashboard')->with(
                 [
-                    'notification'  =>  [
+                    'alert'  =>  [
                         'msg'       =>  'Kode presensi tidak ditemukan, harap coba kembali beberapa saat, atau hubungi admin.',
                         'status'    =>  'failed'
                     ]
                 ]
             );
         }
-        $day = now()->format('l');
-        // if()
-        $registrar = auth()->user()->registrar;
-        $registrar->load(
+
+        // checikng if now is the right time to presence
+        $schedule = $qr->schedule;
+        $day = now()->dayName;
+        $time = now()->format('H:i');
+        $timeState = false;
+        if ($schedule->day == $day && ($schedule->start_time < $time && $schedule->end_time > $time)) {
+            $timeState = true;
+        }
+
+        // finding psr for user
+        $psr = PeriodSubjectRegistrar::query()
+            ->where('registrar_id', auth()->user()->registrar->id)
+            ->where('period_subject_id', $qr->schedule->classroom->period_subject->id)
+            ->with(
+                [
+                    'registrar',
+                    'schedules'
+                ]
+            )
+            ->first();
+
+        // return failed if user is not registered as lab assistant
+        if (!$psr) {
+            return to_route('user.dashboard')->with(
+                [
+                    'alert'  =>  [
+                        'msg'       =>  'Anda tidak terdaftar sebagai asisten di mata kuliah ' . $qr->schedule->classroom->period_subject->subject->name,
+                        'status'    =>  'failed'
+                    ]
+                ]
+            );
+        }
+
+        // validating the class is right
+        $classState = false;
+        // dd($psr->schedules, $qr->schedule);
+        // dd($psr->schedules->contains('id', $qr->schedule->id));
+        if ($psr->schedules->contains('id', $qr->schedule->id)) {
+            $classState = true;
+        }
+        if ($timeState && $classState) {
+            $state = true;
+        }
+
+        // check if lab assistant already presence
+        $presence = Presence::query()
+            ->where('psr_id', $psr->id)
+            ->where('qr_id', $qr->id)
+            ->count();
+        if ($presence > 0) {
+            return to_route('user.dashboard')->with(
+                [
+                    'alert'  =>  [
+                        'msg'       =>  'Anda tidak dapat mealakukan presensi lebih dari 1 kali untuk Mata Kuliah ini',
+                        'status'    =>  'failed'
+                    ]
+                ]
+            );
+        }
+
+        // submit presence
+        $presence = Presence::create(
             [
-                'period_subjects'
+                'psr_id'    =>  $psr->id,
+                'qr_id'     =>  $qr->id,
+                'is_valid'  =>  $state
             ]
         );
-        $psr = PeriodSubjectRegistrar::query()
-            ->where('registrar_id', $registrar->id)
-            ->where('period_subject_id', $qr->schedule->classroom->period_subject->id)
-            ->first();
-        dd($qr);
-        $presence = Presence::query()
-            ->where('psr_id',);
+        if ($presence) {
+            return to_route('user.dashboard')->with(
+                [
+                    'alert'  =>  [
+                        'msg'       =>  'Presensi berhasil dilakukan',
+                        'status'    =>  'success'
+                    ]
+                ]
+            );
+        }
 
-        dd($psr);
-        // if ($registrar) {
-        //     return response()->json(
-        //         [
-        //             'msg'   =>  'Presensi sudah ada'
-        //         ]
-        //     );
-        // }
+        // return unk error
+        return to_route('user.dashboard')->with(
+            [
+                'alert'  =>  [
+                    'msg'       =>  'Presensi gagal dilakukan, harap coba kembail beberapa saat',
+                    'status'    =>  'failed'
+                ]
+            ]
+        );
     }
 
     public function scheduleIndex()
@@ -111,7 +183,7 @@ class UserDashboardController extends Controller
                         );
                     },
                     'schedules' =>  function ($query) {
-                        $query->with('classroom','room');
+                        $query->with('classroom', 'room');
                     },
                 ]
             )->get();
@@ -139,5 +211,24 @@ class UserDashboardController extends Controller
             'period_subjects'   =>  $period_subjects,
             'psrs'  =>  $psrs
         ]);
+    }
+
+    public function scheduleStore(Request $request)
+    {
+        // dd($request->all());
+        // $validated = $request->validate(
+        //     [
+        //         'schedule_id'   =>  'required',
+        //         'psr_id'        =>  'required'
+        //     ]
+        // );
+        // dd($validated);
+        // $schedule = Schedule::with('psrs')->find($validated['schedule_id']);
+        // $psr = PeriodSubjectRegistrar::find($validated['psr_id']);
+        // if($schedule->psrs->contains($psr)){
+
+        // }
+        // $schedule->psrs()->attach($validated['psr_id']);
+        // return
     }
 }
