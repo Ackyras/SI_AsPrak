50336500224a5fc3afa30b32a27fbcc51674c8fa
Togi Stark <markus.118140037@student.itera.ac.id>
2022-08-22 02:58:12 +0700

    Thank you in advance

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -7,7 +7,7 @@
                 {{ user.nim }}
             </p>
 
-            <div class="flex items-center p-4 mb-4 bg-amber-100 rounded-lg" role="alert" v-if="notFinishedExam > 0">
+            <div class="flex items-center p-4 mb-4 bg-amber-100 rounded-lg" role="alert" v-if="notFinishedExam > 0 && period.is_exam_selection_over == 0">
                 <svg aria-hidden="true" class="flex-shrink-0 w-5 h-5 text-amber-700" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path>
                 </svg>
@@ -21,7 +21,7 @@
 
             <!-- <p class="text-lg font-bold mb-2">USER</p>
             <pre class="mb-3 border border-black">
-                {{ JSON.stringify(user, null, "\t") }}
+                {{ JSON.stringify(period, null, "\t") }}
             </pre> -->
         </div>
     </Authenticated>
@@ -37,6 +37,7 @@ export default {
     },
     props: {
         user: Object,
+        period : Object
     },
     // data() {
     //     return {
